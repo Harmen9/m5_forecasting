479312321a53d2616e3310a3215451eb5852098a
Harmen <harmen.kerkenaar@qbis.ch>
2023-12-08 08:52:40 +0100

    Add calendar data

--- code/m5_main.py
+++ code/m5_main.py
@@ -1,13 +1,18 @@
+'''This functions runs the m5 forecasting of the wallmart data.
+The input data can be downloaded from Kaggle and should be stored in the
+input folder. The different configurations can be set in the code/config folder'''
+
 import json
 from datetime import datetime
 import pandas as pd
-from pathlib import Path
 import lightgbm as lgb
-import wandb
-from wandb.lightgbm import wandb_callback, log_summary
 
 from classes import TsPreproc
 from config import *
+
+import wandb
+from wandb.lightgbm import wandb_callback, log_summary
+
 
 # Get the current date and time
 current_datetime = datetime.now().strftime("%Y%m%d%H%M")
@@ -15,7 +20,7 @@ selected_run_id = 'Baseline'
 
 # Read config file
 paths = Paths()
-train_val: pd.DataFrame = pd.read_csv(paths.INPUT / 'sales_train_validation.csv')
+data_grid: pd.DataFrame = pd.read_csv(paths.INPUT / 'sales_train_validation.csv')
 run_params = globals()[selected_run_id]() 
 
 wandb.init(
@@ -37,47 +42,82 @@ with open(output_folder / "run_params.json", 'w', encoding="utf-8") as json_file
 
 # To speed up the training of the data during data exploration only store CA_1 is taken 
 # into account
-store_mask: pd.Series = train_val['store_id'].isin(run_params.SELECTED_STORES)
-train_val: pd.DataFrame = train_val[store_mask]
+store_mask: pd.Series = data_grid['store_id'].isin(run_params.SELECTED_STORES)
+data_grid: pd.DataFrame = data_grid[store_mask]
 del store_mask
-train_val: TsPreproc = TsPreproc(train_val)
+data_grid: TsPreproc = TsPreproc(data_grid)
 
 # Pivot table
-train_val.melt(
+data_grid.melt(
     id_vars=run_params.index_columns,
     var_name='d',
     value_name=run_params.TARGET
 )
 
-train_val.data['d'] = train_val.data['d'].str.replace('d_', '').astype(int)
+# Add calendar, only use the first event
+calendar_columns: list[str] = ['wday', 'month', 'year', 'd', 'event_name_1', 'event_type_1']
+calendar_df: pd.DataFrame = pd.read_csv(paths.INPUT / 'calendar.csv')
+data_grid.data = data_grid.data.merge(
+    right=calendar_df[calendar_columns],
+    how='left',
+    left_on='d',
+    right_on='d'
+    )
+
+data_grid.data['d'] = data_grid.data['d'].str.replace('d_', '').astype(int)
 
 # Get lag features
-train_val.generate_lags(
+data_grid.generate_lags(
     lags=run_params.LAGS,
     group_by=['id'],
     lag_column=run_params.TARGET
     )
 
-categorical_features = ['item_id', 'dept_id', 'cat_id']
-train_val.label_encode(
+
+# Use encoding to encode categorical features.
+categorical_features = [
+    'item_id',
+    'dept_id',
+    'cat_id',
+    'event_name_1',
+    'event_type_1'
+    ]
+data_grid.label_encode(
     categorical_columns=categorical_features
     )
 
 
-# Split data
-MAX_D = max(train_val.data['d'])
-END_TRAIN = MAX_D - run_params.TRAIN_SPLIT
-mask_train = train_val.data['d']<=END_TRAIN
 
+# Split data
+MAX_D = max(data_grid.data['d'])
+END_TRAIN = MAX_D - run_params.TRAIN_SPLIT
+mask_train = data_grid.data['d']<=END_TRAIN
+
+# Create train and val datasets
 train_data = lgb.Dataset(
-    data=train_val.data.drop(columns=['id', 'store_id', 'state_id', run_params.TARGET])[mask_train],
-    label=train_val.data['sales'][mask_train]
+    data=data_grid.data.drop(
+        columns=[
+            'id',
+            'store_id',
+            'state_id',
+            run_params.TARGET
+            ]
+        )[mask_train],
+    label=data_grid.data['sales'][mask_train]
     )
+
 val_data = lgb.Dataset(
-    data=train_val.data.drop(columns=['id', 'store_id', 'state_id', run_params.TARGET])[~mask_train],
-    label=train_val.data['sales'][~mask_train]
+    data=data_grid.data.drop(
+        columns=[
+            'id',
+            'store_id',
+            'state_id',
+            run_params.TARGET
+            ]
+        )[~mask_train],
+    label=data_grid.data['sales'][~mask_train]
     )
-del train_val
+del data_grid
 # Initialize the callback to record evaluation metrics
 
 evals={}
